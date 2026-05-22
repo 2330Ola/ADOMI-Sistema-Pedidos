5c4cb3b81be94f37a50bfdcc47c64acbc6fcd60a
2330Ola <jolao@miumg.edu.gt>
2026-05-22 00:34:04 -0600

    Configurar frontend para backend online

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api"
+    baseURL: "https://adomi-sistema-pedidos.onrender.com/api"
 });
 
 export default api;
