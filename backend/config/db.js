6470aab46fcf5c170a734dcebc4a265ebca79637
2330Ola <jolao@miumg.edu.gt>
2026-05-21 23:10:12 -0600

    Configurar base de datos para deploy

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,19 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "adomi_db"
+    host: process.env.MYSQLHOST || "localhost",
+    port: process.env.MYSQLPORT || 3306,
+    user: process.env.MYSQLUSER || "root",
+    password: process.env.MYSQLPASSWORD || "",
+    database: process.env.MYSQLDATABASE || "adomi_db"
 });
 
 connection.connect((err) => {
-
-    if(err){
-        console.log("Error de conexión:", err);
+    if (err) {
+        console.log("Error de conexión MySQL:", err);
     } else {
         console.log("MySQL conectado");
     }
-
 });
 
 module.exports = connection;
